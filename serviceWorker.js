8ab032155a8b7761ebd435c972b334140ddf6fb1
Ric3cir121 <ric3cir121@gmail.com>
2024-10-09 19:33:41 +0200

    Made pages available offline

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,20 @@
-// This worker is empty
+self.addEventListener('install', event => {
+    event.waitUntil((async ()=>{
+        await caches.delete('availableOffline');
+        (await caches.open('availableOffline')).addAll(['/', '/index.hmtl', '/kotoli.json', '/manifest.json']);
+    })());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith((async ()=>{
+        const cache = await caches.open('availableOffline');
+
+        // Try the cache first.
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse !== undefined) {
+            return cachedResponse;
+        } else {
+            return fetch(event.request);
+        }
+    })())
+})
