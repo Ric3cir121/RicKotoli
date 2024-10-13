cca76ec917feb5e79b7fd594250d76fad3b901f6
Ric3cir121 <ric3cir121@gmail.com>
2024-10-13 19:17:20 +0200

    Added search algorithm

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ var kotoli = {
     kotoli: []
 };
 
+function levenshteinDistance(a, b){
+    let aLen = a.length+1, bLen = b.length+1;
+    let board = [];
+    for(let i=0;i<aLen*bLen;i++)board.push(0);
+
+    for(let i=1;i<aLen;i++) board[i] = i;
+    for(let j=1;j<bLen;j++) board[j*aLen] = j;
+  
+    for(let i=1;i<aLen;i++) for(let j=1;j<bLen;j++){
+        board[i + j*aLen] = Math.min(Math.min(board[i-1 + j*aLen]     + 1,
+                                              board[i   + (j-1)*aLen] + 1),
+                                              board[i-1 + (j-1)*aLen] + (a[i-1] != b[j-1]));
+    }
+
+    return board[aLen-1 + (bLen-1)*aLen];
+}
+
 function spawnElements(){
     if ('serviceWorker' in navigator){
         navigator.serviceWorker.register('serviceWorker.js');
@@ -12,6 +29,7 @@ function spawnElements(){
     let searchBar = document.createElement('input');
     searchBar.classList.add("searchBar");
     searchBar.placeholder = "Da sukha...";
+    searchBar.oninput = updateSearch;
     topBar.appendChild(searchBar);
 
     document.body.querySelector('#mainContent').appendChild(topBar);
@@ -31,21 +49,39 @@ function sanitize(text){
     return text.replace('<', '&lt').replace('>', '&gt').replace('&', '&amp')
 }
 function updateSearch(){
+    let input = document.querySelector('.searchBar').value ?? "";
+    kotobaraLibre = kotoli.kotoli.slice()
+    if(input){
+        let kotobaScore = {}
+        for(result of kotobaraLibre){
+            score = Infinity;
+            for(kotoba of result.kotobara)
+                score = Math.min(score, levenshteinDistance(input.toLowerCase(), kotoba.toLowerCase()) - (kotoba.length - input.length) * .6);
+            kotobaScore[result.kotobara[0]] = score;
+        }
+        kotobaraLibre.sort((a,b) => (kotobaScore[a.kotobara[0]] > kotobaScore[b.kotobara[0]]) -.5);
+        let badScore = 0;
+        for(badScore=0; badScore<kotobaraLibre.length; badScore++)
+            if(kotobaScore[kotobaraLibre[badScore].kotobara[0]] > 3)break;
+        kotobaraLibre = kotobaraLibre.slice(0,badScore);
+    } else {
+        kotobaraLibre.sort((a,b) => (a.kotobara[0].toLowerCase() > b.kotobara[0].toLowerCase()) -.5);
+    }
     let searchResultsContainer = document.querySelector('#searchResultsContainer');
     searchResultsContainer.innerHTML = '';
-    for(let i=0; i<kotoli.kotoli.length; i++){
+    for(let i=0; i<kotobaraLibre.length; i++){
         let searchResult = document.createElement('div');
         searchResult.classList.add("searchResult");
-        let kotoba = '<div class="kotoba">' + sanitize(kotoli.kotoli[i].kotobara[0]) + '</div>';
+        let kotoba = '<div class="kotoba">' + sanitize(kotobaraLibre[i].kotobara[0]) + '</div>';
 
         let alRisonen = '';
-        for(risonen of kotoli.kotoli[i].risonen) alRisonen += sanitize(risonen);
+        for(risonen of kotobaraLibre[i].risonen) alRisonen += sanitize(risonen);
         alRisonen = '<div class="risonen">' + alRisonen + '</div>';
 
         searchResult.innerHTML += '<div class="kotobaTop">' + kotoba + alRisonen + '</div>';
 
         let alKotobara = '';
-        for(kotobara of kotoli.kotoli[i].kotobara.slice(1)) alKotobara += sanitize(kotobara) + ", ";
+        for(kotobara of kotobaraLibre[i].kotobara.slice(1)) alKotobara += sanitize(kotobara) + ", ";
         if(alKotobara.length >= 2)alKotobara = alKotobara.slice(0,-2);
         searchResult.innerHTML += '<div class="kotobara">' + alKotobara + '</div>';
 
@@ -147,7 +183,6 @@ function autoSetTheme(){
 }
 async function updateKotoli(){
     kotoli = await (await fetch('kotoli.json')).json();
-    kotoli.kotoli.sort((a,b) => (a.kotobara[0].toLowerCase() > b.kotobara[0].toLowerCase()) -.5);
     updateSearch();
 }
 
